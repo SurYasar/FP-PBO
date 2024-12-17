65f7b2c81d5c8c9828911cf1a006c9a21525651e
SurYasar <57575414+SurYasar@users.noreply.github.com>
2024-12-17 13:29:07 +0700

    update Inheritance & Polymorph for RangedEnemy

--- Assets/Script/RangedEnemy.cs
+++ Assets/Script/RangedEnemy.cs
@@ -2,13 +2,8 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class RangedEnemy : MonoBehaviour
+public class RangedEnemy : Enemy
 {
-    public Transform target;
-    public float speed = 3f;
-    public float rotateSpeed = 0.02f;
-    public Rigidbody2D rb;
-
     public GameObject enemyBulletPrefab;
     public float distanceToShoot = 5f;
     public float distanceToStop = 3f;
@@ -17,29 +12,26 @@ public class RangedEnemy : MonoBehaviour
     public float fireRate;
     public Transform firePoint;
 
-    public AudioClip hitsfx;
-    public AudioClip playerHit;
     public AudioClip shootSFX;
 
-    private void Start()
+    protected override void Start()
     {
         rb = GetComponent<Rigidbody2D>();
     }
 
-    private void Update()
+    protected override void Update()
     {
-        if (!target)
+        base.Update();
+
+        if (target == null)
         {
             GetTarget();
+            return;
         }
-        else
-        {
-            RotateTowardsTarget();
 
-            if (Vector2.Distance(target.position, transform.position) <= distanceToShoot)
-            {
-                Shoot();
-            }
+        if (Vector2.Distance(target.position, transform.position) <= distanceToShoot)
+        {
+            Shoot();
         }
 
         
@@ -59,47 +51,44 @@ public class RangedEnemy : MonoBehaviour
         }
 
     }
-    private void FixedUpdate()
+    protected override void FixedUpdate()
     {
-        if (target != null)
+        if (target == null)
+            return;
+
+        base.FixedUpdate();
+
+        if (Vector2.Distance(target.position, transform.position) >= distanceToStop)
         {
-            if (Vector2.Distance(target.position, transform.position) >= distanceToStop)
-            {
-                rb.velocity = transform.up * speed;
-            }
-            else
-            {
-                rb.velocity = Vector2.zero;
-            }
+            rb.velocity = transform.up * speed;
         }
+        else
+        {
+            rb.velocity = Vector2.zero;
+        }
+        
         
     }
 
-    private void RotateTowardsTarget()
+    protected override void GetTarget()
     {
-        Vector2 targetDirection = target.position - transform.position;
-        float angle = Mathf.Atan2(targetDirection.y, targetDirection.x) * Mathf.Rad2Deg - 90f;
-        Quaternion q = Quaternion.Euler(new Vector3(0, 0, angle));
-        transform.localRotation = Quaternion.Slerp(transform.localRotation, q, rotateSpeed);
-    }
-    private void GetTarget()
-    {
-        if (GameObject.FindGameObjectWithTag("Player"))
+        GameObject player = GameObject.FindGameObjectWithTag("Player");
+        if (player != null)
         {
-            target = GameObject.FindGameObjectWithTag("Player").transform;
+            target = player.transform;
+        }
+        else
+        {
+            Debug.LogWarning("Player not found! Make sure an object with tag 'Player' exists in the scene.");
         }
     }
 
-    private void OnCollisionEnter2D(Collision2D other)
+
+    protected override void OnCollisionEnter2D(Collision2D other)
     {
-        if (other.gameObject.CompareTag("Player"))
-        {
-            LevelManager.manager.GameOver();
-            AudioManager.instance.PlaySFX(playerHit);
-            Destroy(other.gameObject);
-            target = null;
-        }
-        else if (other.gameObject.CompareTag("Bullet"))
+        base.OnCollisionEnter2D(other);
+
+        if (other.gameObject.CompareTag("Bullet"))
         {
             LevelManager.manager.IncreaseScore(3);
             Destroy(other.gameObject);
